77f89c303ded581eab9ba40d613996bc2dc6eb19
amayuru56789 <indeewara5678@gmail.com>
2022-04-21 20:34:49 +0530

    added dom objects for game

---
+++ assets/js/nfs_script.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    var anim_id;
+
+    /*saving dom objects to variables*/
+    var container = $('#container');
+    var car = $('#car');
+    var car_1 = $('#car_1');
+    var car_2 = $('#car_2');
+    var car_3 = $('#car_3');
+    var line_1 = $('#line_1');
+    var line_2 = $('#line_2');
+    var line_3 = $('#line_3');
+    var restart_div = $('#restart_div');
+    var restart_btn = $('#restart');
+    var score = $('#score');
+    var high_score = localStorage.getItem('high_score');
+    $('#high_score').text(high_score);
+
+
+
+
+});
